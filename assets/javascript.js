dc55d4a0956b3fcc66ce0305cd7e2f5e03dd2cf9
Whitley Blaine <whitleyblaine@gmail.com>
2016-06-11 16:01:57 -0400

    Added load more gifs button

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,8 @@
 // Variables
 var rapperList = ["Lil Wayne", "Kendrick Lamar", "Ghostface Killer", "Lauryn Hill", "Chance the Rapper", "Andre 3000", "Nas", "Rakim", "Eminem", "Jay Z", "Biggie Smalls", "Kanye West"]
+var queryTerm;
+
+// Functions
 
 var makeButtons = function() {
   $('#rapperButtons').empty();
@@ -10,27 +13,13 @@ var makeButtons = function() {
   }
 }
 
-$(document).ready(function() {
-  makeButtons();
-});
-
-
-$('#addRapper').on('click', function() {
-  var toAdd = $('#rapper-input').val();
-  rapperList.push(toAdd);
-  makeButtons();
-  // Don't refresh the page!
-  return false;
-});
-
-$(document).on('click', '.rapperButton', function() {
-  var queryTerm = $(this).text();
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryTerm + "&limit=10&api_key=dc6zaTOxFJmzC";
+var ajaxCall = function(queryTerm, numGifs) {
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryTerm + "&limit=" + numGifs + "&api_key=dc6zaTOxFJmzC";
 
   $.ajax({url: queryURL, method: 'GET'})
   .done(function(response) {
     console.log(response);
-    for(i=0; i < 10; i++) {
+    for(i=0; i < numGifs; i++) {
       var stillURL = response.data[i].images.fixed_width_still.url;
       var movingURL = response.data[i].images.fixed_width.url;
       var rating = response.data[i].rating;
@@ -48,8 +37,37 @@ $(document).on('click', '.rapperButton', function() {
       $('#td' + i).html(gifDiv);
     };
   });
+}
+
+// Load initial buttons
+$(document).ready(function() {
+  makeButtons();
 });
 
+// Add a rapper button by clicking submit
+$('#addRapper').on('click', function() {
+  var toAdd = $('#rapper-input').val();
+  rapperList.push(toAdd);
+  makeButtons();
+  // Don't refresh the page!
+  return false;
+});
+
+// perform ajax call to retrieve gifs by clicking desired rapper button
+$(document).on('click', '.rapperButton', function() {
+  queryTerm = $(this).text();
+  $('td').html('');
+  $('#loadMore').removeClass('hide');
+
+  ajaxCall(queryTerm, 10);
+
+  $(document).on('click', '#loadMore', function() {
+    $('#loadMore').addClass('hide');
+    ajaxCall(queryTerm, 20);
+  });
+});
+
+// pause & restart gif on click
 $(document).on('click', '.rapperImage', function() {
   currentSrc = $(this).attr('src');
   newSrc = $(this).attr('data-alt');
